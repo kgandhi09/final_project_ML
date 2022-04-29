4b8ce883cd7f7b0f0fc013a5929caf6cca3787a9
kgandhi09 <kushal009gandhi@gmail.com>
2022-04-29 16:57:53 -0400

    writing data specs

---
+++ Readme.m
@@ -0,0 +1,9 @@
+Input Factors:
+->stock_prices.csv:
+    -> Open, Close, Low, High
+->options.csv
+    -> WholeDayOpen, WholeDayHigh, WholeDayLow, WholeDayClose, NightSessionOpen, NightSessionHigh, NightSessionLow, NightSessionClose, DaySessionClose, DaySessionHigh, DaySessionLow, DaySessionOpen, TradingVolume, OpenInterest, TradingValue, StrikePrice, WholeDayVolume
+
+Output:
+-> Expected Dividend
+
