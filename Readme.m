4ee876694316a54b586f689aebdba24b25e3a6ac
kgandhi09 <kushal009gandhi@gmail.com>
2022-04-29 17:18:52 -0400

    added some links for reference for deep neural network

--- Readme.m
+++ Readme.m
@@ -7,3 +7,13 @@ Input Factors:
 Output:
 -> Expected Dividend
 
+
+-> For Shallow Model:
+    ->
+
+-> For 3-layered NN:
+    -> 
+
+-> For Deep Neural Network:
+    -> https://journalofbigdata.springeropen.com/articles/10.1186/s40537-020-00333-6
+    -> https://towardsdatascience.com/aifortrading-2edd6fac689d
