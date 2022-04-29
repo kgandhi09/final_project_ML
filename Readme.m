f4a2b44650a24d2580afebb811c80026fa24114f
kgandhi09 <kushal009gandhi@gmail.com>
2022-04-29 17:21:55 -0400

    added a new paper to refer for deep neural network model

--- Readme.m
+++ Readme.m
@@ -17,3 +17,4 @@ Output:
 -> For Deep Neural Network:
     -> https://journalofbigdata.springeropen.com/articles/10.1186/s40537-020-00333-6
     -> https://towardsdatascience.com/aifortrading-2edd6fac689d
+    -> https://www.researchgate.net/publication/343923265_Stock_Price_Forecasting_with_Deep_Learning_A_Comparative_Study
