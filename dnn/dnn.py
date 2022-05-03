96fc90d7e30950a99e01100e96157c107a847c9e
kgandhi09 <kushal009gandhi@gmail.com>
2022-05-03 15:54:53 -0400

    done with the final project and school and WPI

--- dnn/dnn.py
+++ dnn/dnn.py
@@ -4,7 +4,9 @@ import keras
 from keras.layers import LSTM, Dense, Dropout
 from keras.models import Sequential
 from keras.utils.vis_utils import plot_model
+from keras import metrics
 from sklearn.preprocessing import MinMaxScaler
+import kaggle
 
 def df_to_ds(dataframe, input_features, label=None):
     input_data = []
@@ -34,10 +36,7 @@ def build_RNN(input_shape_):
     #Adding the third LSTM layer and some Dropout Regularization
     model.add(LSTM(units = 50, return_sequences = True))
     model.add(Dropout(0.2))
-    #Adding the FOURTH LSTM layer and some Dropout Regularization
-    model.add(LSTM(units = 50, return_sequences = True))
-    model.add(Dropout(0.2))
-    #Adding the fifth LSTM layer and some Dropout Regularization
+    #Adding the fourth LSTM layer and some Dropout Regularization
     model.add(LSTM(units = 50, return_sequences = False))
     model.add(Dropout(0.2))
     #Adding the output layer
@@ -51,7 +50,7 @@ if __name__ == "__main__":
     label = ['Target']
 
     df = pd.read_csv("../train_files/stock_prices.csv")
-    #df = df.iloc[:5,:]
+    # df = df.iloc[:5,:]
     test_df = pd.read_csv("../supplemental_files/stock_prices.csv")
     
     df.dropna(subset=input_features + label, axis=0, inplace=True)
@@ -66,13 +65,19 @@ if __name__ == "__main__":
     model = build_RNN((input_train.shape[1], 1))
 
     #Compiling the RNN
-    model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics=['accuracy'])
+    model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics=['accuracy', metrics.RootMeanSquaredError()])
 
     model.summary()
-    plot_model(model, show_shapes=True, rankdir="LR")
+    plot_model(model, show_shapes=True, rankdir="TB")
 
     #Fitting the RNN to the Training set
     model.fit(input_train, label_train, epochs = 3)  
     model.save('stock_prediction_rnn.h5')
 
     stock_prediction_rnn = model.predict(X_test)
+    stock_prediction_rnn = np.reshape(stock_prediction_rnn, (stock_prediction_rnn.shape[0],))
+    test_df_predictions = test_df
+    test_df_predictions = test_df_predictions.drop(columns=['Target'])
+    test_df_predictions['Target'] = stock_prediction_rnn.tolist()
+
+    df_csv = test_df_predictions.to_csv('df_csv.csv')
