4192edc51d6b6d6caa1bffc548bb10c1c90e199d
kgandhi09 <kushal009gandhi@gmail.com>
2022-05-03 07:16:08 -0400

    organized and restructured. Need to write rank function

--- dnn/dnn.py
+++ dnn/dnn.py
@@ -6,53 +6,27 @@ from keras.models import Sequential
 from keras.utils.vis_utils import plot_model
 from sklearn.preprocessing import MinMaxScaler
 
-def df_to_ds(dataframe, input_features, label):
+def df_to_ds(dataframe, input_features, label=None):
     input_data = []
     for feature in input_features:
         input_data.append(dataframe[feature].to_numpy())
     input_data = np.array(input_data).T
     input_data = np.reshape(input_data, (input_data.shape[0], input_data.shape[1], 1))
-    label = dataframe[label[0]].to_numpy()
-    # label = np.reshape(label, (label.shape[0], 1))
+    if label:
+        label = dataframe[label[0]].to_numpy()
     return input_data, label
 
-if __name__ == "__main__":
-    df = pd.read_csv("../train_files/stock_prices.csv")
-    df = df.sort_values("SecuritiesCode")
-    securities_count = df["SecuritiesCode"].value_counts()
-    securities = securities_count.to_dict()
-    
-    list_df = []
-    temp = df
-    
-    for security in securities:
-        temp = df[df["SecuritiesCode"] == security].copy()
-        temp = temp.sort_values("Date")
-        list_df.append(temp)
-    
-    #separating training and validation data
-    train_ratio = 0.8
-    
-    train_df_list = []
-    val_df_list = []
-    
-    for df in list_df:
-        train_df_list.append(df.iloc[:int(df.shape[0]*train_ratio),:])
-        val_df_list.append(df.iloc[int(df.shape[0]*train_ratio):,:])
+def train_val_split(dataframe, train_ratio):
+    train_df = dataframe.iloc[:int(dataframe.shape[0]*train_ratio),:]
+    val_df = dataframe.iloc[int(dataframe.shape[0]*train_ratio):,:]
+    return train_df, val_df
 
-    input_features = ['Open', 'High', 'Low', 'Close', 'Volume']
-    label = ['Target']
-
-    train_df_list[0].dropna(subset=input_features + label, axis=0, inplace=True)
-
-    input_train, label_train = df_to_ds(train_df_list[0], input_features, label)
-    input_val, label_val = df_to_ds(val_df_list[0], input_features, label)
-
+def build_RNN(input_shape_):
     #Initializing the RNN
     model = Sequential()
     #Making a robust stacked LSTM layer
     #Adding the first LSTM layer and some Dropout Regularization
-    model.add(LSTM(units = 50, return_sequences = True, input_shape = (input_train.shape[1], 1) ) )
+    model.add(LSTM(units = 50, return_sequences = True, input_shape = input_shape_))
     model.add(Dropout(0.2))
     #Adding the second LSTM layer and some Dropout Regularization
     model.add(LSTM(units = 50, return_sequences = True))
@@ -60,12 +34,36 @@ if __name__ == "__main__":
     #Adding the third LSTM layer and some Dropout Regularization
     model.add(LSTM(units = 50, return_sequences = True))
     model.add(Dropout(0.2))
-    #Adding the fourth LSTM layer and some Dropout Regularization
+    #Adding the FOURTH LSTM layer and some Dropout Regularization
+    model.add(LSTM(units = 50, return_sequences = True))
+    model.add(Dropout(0.2))
+    #Adding the fifth LSTM layer and some Dropout Regularization
     model.add(LSTM(units = 50, return_sequences = False))
     model.add(Dropout(0.2))
-
     #Adding the output layer
     model.add(Dense(units = 1))
+
+    return model
+
+if __name__ == "__main__":
+    
+    input_features = ['Open', 'High', 'Low', 'Close', 'Volume']
+    label = ['Target']
+
+    df = pd.read_csv("../train_files/stock_prices.csv")
+    #df = df.iloc[:5,:]
+    test_df = pd.read_csv("../supplemental_files/stock_prices.csv")
+    
+    df.dropna(subset=input_features + label, axis=0, inplace=True)
+    test_df.dropna(subset=input_features, axis=0, inplace=True)
+
+    train_df,val_df = train_val_split(df, 0.8)
+
+    input_train, label_train = df_to_ds(train_df, input_features, label=label)
+    input_val, label_val = df_to_ds(val_df, input_features, label=label)
+    X_test, label = df_to_ds(test_df, input_features)
+
+    model = build_RNN((input_train.shape[1], 1))
 
     #Compiling the RNN
     model.compile(optimizer = 'adam', loss = 'mean_squared_error', metrics=['accuracy'])
@@ -74,5 +72,7 @@ if __name__ == "__main__":
     plot_model(model, show_shapes=True, rankdir="LR")
 
     #Fitting the RNN to the Training set
-    model.fit(input_train, label_train, epochs = 3, valdiation_data = (input_val, label_val))  
+    model.fit(input_train, label_train, epochs = 3)  
     model.save('stock_prediction_rnn.h5')
+
+    stock_prediction_rnn = model.predict(X_test)
